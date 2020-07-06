5dcfb1e444000bc27775859e01494a5183d66ac6
Hansen0314 <595355940@qq.com>
2020-07-06 08:08:19 +0000

    fix:env error for env_install.sh

--- env_install.sh
+++ env_install.sh
@@ -11,35 +11,35 @@ cd /bin/
 wget https://raw.githubusercontent.com/turmary/EMSTS/master/scripts/bbb/gpio
 chmod 775 /bin/gpio
 GPIO_CHECK=`gpio`
-if [ -n $GPIO_CHECK ]
+if [ $GPIO_CHECK ]
 then
-    ehco "cannot find gpio please check whether installed gpio successfully"
+    echo "cannot find gpio please check whether installed gpio successfully"
     exit 1
 fi
 
 # install phytool
-cd ~;git clone https://github.com/wkz/phytool
-cd ~/phytool
+cd /home/debian;git clone https://github.com/wkz/phytool
+cd /home/debian/phytool
 make
 make install
 PHYTOOL_CHECK=`phytool | tail -n 1`
-if [  -n $PHYTOOL_CHECK ]
+if [ $PHYTOOL_CHECK ]
 then
-    ehco "cannot find phytool please check whether installed gpio successfully"
+    echo "cannot find phytool please check whether installed phytool successfully"
     exit 1
 fi
 
 # install power-service
-cd ~/aurora_test/power-service
+cd aurora_test/power-service
 bash install.sh
 
 # install voice-card
 
 sh -c "echo uboot_overlay_addr0=/lib/firmware/BB-BONE-AUDI-02-00A0.dtbo >> /boot/uEnv.txt"
-cd ~/aurora_test/voice_card
+cd aurora_test/voice_card
 alsactl restore 0 -f asound.state
 cp asound.conf  /etc/asound.conf
-ehco "reboot to enable voice config"
-sleep 1
+echo "reboot to enable voice config"
+sleep 10
 reboot
 
