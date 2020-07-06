3796c7f7a27edbdf4fb0701875053c18a8b28eb0
Hansen0314 <595355940@qq.com>
2020-07-06 14:51:07 +0800

    fix:error path for gpio

--- power-service/power_maneger.sh
+++ power-service/power_maneger.sh
@@ -3,18 +3,18 @@ COUNTER=0
 SOMOOTH_FACTOR=3
 while true
 do
-    WDOG_VAULE=`/opt/EMSTS/scripts/bbb/gpio input 49` ##get mpu single
+    WDOG_VAULE=`/bin/gpio input 49` ##get mpu single
     if [ $WDOG_VAULE = 1 ] ##if WDOG_VAULE is high
     then
         COUNTER=`expr $COUNTER + 1`
     else
         COUNTER=0
     fi
-    if [ $COUNTER -ge $SOMOOTH_FACTOR ] ##if counter > 5
+    if [ $COUNTER -ge $SOMOOTH_FACTOR ] ##if counter > 3
     then
-        /opt/EMSTS/scripts/bbb/gpio set 48  ##feedback to mpu
+        /bin/gpio set 48  ##feedback to mpu
         sleep 0.2
-        /opt/EMSTS/scripts/bbb/gpio clear 48  ##feedback to mpu
+        /bin/gpio clear 48  ##feedback to mpu
         echo "get feedback from mpu"
         COUNTER=0
     fi
