85349c7732b7f947a60bf50bc3e80343eb74b20a
Hansen0314 <595355940@qq.com>
2020-07-07 10:47:50 +0800

    fix:gpio cannot install

--- env_install.sh
+++ env_install.sh
@@ -6,34 +6,24 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 # install gpio
-GPIO_CHECK=`gpio`
-if [ $GPIO_CHECK ]
-then
-    cd /bin/
-    wget https://raw.githubusercontent.com/turmary/EMSTS/master/scripts/bbb/gpio
-    chmod 775 /bin/gpio
-    GPIO_CHECK=`gpio`
-fi
+cd /bin/
+wget https://raw.githubusercontent.com/turmary/EMSTS/master/scripts/bbb/gpio
+chmod 775 /bin/gpio
 
 # install phytool
-PHYTOOL_CHECK=`phytool | tail -n 1`
-if [ $PHYTOOL_CHECK ]
-then
-    cd /home/debian;git clone https://github.com/wkz/phytool
-    cd /home/debian/phytool
-    make
-    make install
-    cp /usr/local/bin/phytool /home/debian/aurora_test/eth0/
-fi
+cd /home/debian;git clone https://github.com/wkz/phytool
+cd /home/debian/phytool
+make
+make install
+cp /usr/local/bin/phytool /home/debian/aurora_test/eth0/
 
 # install power-service
-cd aurora_test/power-service
+cd /home/debian/aurora_test/power-service
 bash install.sh
 
 # install voice-card
-
 sh -c "echo uboot_overlay_addr0=/lib/firmware/BB-BONE-AUDI-02-00A0.dtbo >> /boot/uEnv.txt"
-cd aurora_test/voice_card
+cd /home/debian/aurora_test/voice_card
 alsactl restore 0 -f asound.state
 cp asound.conf  /etc/asound.conf
 echo "reboot to enable voice config"
